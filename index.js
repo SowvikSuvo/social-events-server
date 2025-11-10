a09ce13cda94ddc9b78e56a716ccd7d73d29b11e
SowvikSuvo <skarmoker44@gmail.com>
2025-11-10 22:52:39 +0600

    added more implement

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ const verifyToken = async (req, res, next) => {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     const db = client.db("social_db");
     const eventsCollection = db.collection("events");
@@ -232,7 +232,7 @@ async function run() {
       }
     });
 
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
